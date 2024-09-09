f540a4049a4294b9b5388991259536c7b87f60ec
riteshr19 <157503168+riteshr19@users.noreply.github.com>
2024-09-09 19:52:32 -0400

    Add files via upload

---
+++ assignment-0.py
@@ -0,0 +1,12 @@
+def add_numbers(a, b):
+    return a + b
+
+# Define two numbers
+num1 = 5
+num2 = 7
+
+# Calculate the sum
+sum_result = add_numbers(num1, num2)
+
+# Print the sum
+print(f"The sum of {num1} and {num2} is {sum_result}")
